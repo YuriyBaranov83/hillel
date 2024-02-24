c3edaacf6bb27d49bb7a67696371123db3b52285
Yuriy Baranov <yuri.baranov83@gmail.com>
2024-02-25 01:07:30 +0200

    fix

--- work1_calc/index.js
+++ work1_calc/index.js
@@ -1,25 +1,31 @@
-const question = prompt('what do you want? please enter: add, sub, mult or div');
+const question = prompt('please enter only: add, sub, mult or div');
 const firstNumber = +prompt('please enter first number');
 const secondNumber = +prompt('please enter second number');
 
 if (typeof firstNumber === "number" && typeof secondNumber === "number") {
     if(question === 'add'){
-        alert(firstNumber + secondNumber);
+        const add = firstNumber + secondNumber;
+        alert(firstNumber + '+' + secondNumber + '=' + add);
     }
-    if(question === 'sub'){
-        alert(firstNumber - secondNumber);
+    else if(question === 'sub'){
+        const sub = firstNumber - secondNumber;
+        alert(firstNumber + '-' + secondNumber + '=' + sub);
     }
-    if(question === 'mult'){
-        alert(firstNumber * secondNumber);
-    }if(question === 'div'){
+    else if(question === 'mult'){
+        const mult = firstNumber * secondNumber;
+        alert(firstNumber + '*' + secondNumber + '=' + mult);
+    }
+    else if(question === 'div'){
         if (secondNumber !== 0) {
-            alert(firstNumber / secondNumber);
+            const div = firstNumber / secondNumber; 
+            alert(firstNumber +  '/' + secondNumber + '=' + div);
         } else {
             alert('error! you cant divide by 0');
             location.reload();
         }
     }else{
         alert('you enter unknown function');
+        location.reload();
     }
 }else{
     alert('next time enter a number');
