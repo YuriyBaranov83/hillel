5886bed40251eb3817c458061588c6d9766e2663
Yuriy Baranov <yuri.baranov83@gmail.com>
2024-02-25 11:37:22 +0200

    fix

--- work1_calc/index.js
+++ work1_calc/index.js
@@ -1,33 +1,37 @@
 const question = prompt('please enter only: add, sub, mult or div');
-const firstNumber = +prompt('please enter first number');
-const secondNumber = +prompt('please enter second number');
+if(!(question === 'add' || question === 'sub' || question === 'mult' || question === 'div')){
+    alert('you enter unknown function');
+    location.reload();
+}else{
+    const firstNumber = +prompt('please enter first number');
+    const secondNumber = +prompt('please enter second number');
 
-if (typeof firstNumber === "number" && typeof secondNumber === "number") {
-    if(question === 'add'){
-        const add = firstNumber + secondNumber;
-        alert(firstNumber + '+' + secondNumber + '=' + add);
-    }
-    else if(question === 'sub'){
-        const sub = firstNumber - secondNumber;
-        alert(firstNumber + '-' + secondNumber + '=' + sub);
-    }
-    else if(question === 'mult'){
-        const mult = firstNumber * secondNumber;
-        alert(firstNumber + '*' + secondNumber + '=' + mult);
-    }
-    else if(question === 'div'){
-        if (secondNumber !== 0) {
-            const div = firstNumber / secondNumber; 
-            alert(firstNumber +  '/' + secondNumber + '=' + div);
-        } else {
-            alert('error! you cant divide by 0');
-            location.reload();
+    if (typeof firstNumber === 'number' && typeof secondNumber === 'number') {
+        if(question === 'add'){
+            const add = firstNumber + secondNumber;
+            alert(firstNumber + '+' + secondNumber + '=' + add);
+        }
+        else if(question === 'sub'){
+            const sub = firstNumber - secondNumber;
+            alert(firstNumber + '-' + secondNumber + '=' + sub);
+        }
+        else if(question === 'mult'){
+            const mult = firstNumber * secondNumber;
+            alert(firstNumber + '*' + secondNumber + '=' + mult);
+        }
+        else if(question === 'div'){
+            if (secondNumber !== 0) {
+                const div = firstNumber / secondNumber; 
+                alert(firstNumber +  '/' + secondNumber + '=' + div);
+            } else {
+                alert('error! you cant divide by 0');
+                location.reload();
+            }
         }
     }else{
-        alert('you enter unknown function');
+        alert('next time enter a number');
         location.reload();
     }
-}else{
-    alert('next time enter a number');
-    location.reload();
 }
+
+
