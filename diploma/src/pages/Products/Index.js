6321cc62d8a8b217628a263745e2bce7de8f121d
Yuriy Baranov <yuri.baranov83@gmail.com>
2024-06-06 08:33:00 +0300

    add part 2

--- diploma/src/pages/Products/Index.js
+++ diploma/src/pages/Products/Index.js
@@ -1,12 +1,15 @@
 import "./Index.css";
 import { ReactComponent as LogoOther } from "../../assets/logo.svg";
+import { GrUserManager } from "react-icons/gr";
+import { GoPlus } from "react-icons/go";
+import { PiArrowsDownUpThin } from "react-icons/pi";
+import { BsFillPencilFill } from "react-icons/bs";
+import { MdDelete } from "react-icons/md";
 import Row from "../../components/Row/Index";
 import Wrapper from "../../components/Wrapper/Index";
 import ButtonLogin from "../../components/ButtonLogin/Index";
-import { GrUserManager } from "react-icons/gr";
-import { GoPlus } from "react-icons/go";
 import Title from "../../components/Title/Index";
-
+import Table from "./components/Table/Index";
 const Products = () => {
   return (
     <div className="products">
@@ -24,6 +27,64 @@ const Products = () => {
         </ButtonLogin>
       </Wrapper>
       <Title>Products</Title>
+      <Table classNameCustom="products-table">
+        <thead>
+          <tr>
+            <th>
+              ID <PiArrowsDownUpThin />
+            </th>
+            <th>
+              Category
+              <PiArrowsDownUpThin />
+            </th>
+            <th>
+              Name
+              <PiArrowsDownUpThin />
+            </th>
+            <th>
+              Quantity
+              <PiArrowsDownUpThin />
+            </th>
+            <th>
+              Price (₴)
+              <PiArrowsDownUpThin />
+            </th>
+          </tr>
+        </thead>
+        <tr>
+          <td>0</td>
+          <td>PC</td>
+          <td>Lenovo Y50-70</td>
+          <td>5</td>
+          <td>25,000.00</td>
+          <td>
+            <BsFillPencilFill />
+            <MdDelete />
+          </td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>Clothes</td>
+          <td>Nike M Nk Df Acd21</td>
+          <td>22</td>
+          <td>4,000.00</td>
+          <td>
+            <BsFillPencilFill />
+            <MdDelete />
+          </td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Plumbing</td>
+          <td>CERSANIT MITO 17</td>
+          <td>1337</td>
+          <td>5,000.00</td>
+          <td>
+            <BsFillPencilFill />
+            <MdDelete />
+          </td>
+        </tr>
+      </Table>
     </div>
   );
 };
--- diploma/src/pages/Products/components/Table/Index.js
+++ diploma/src/pages/Products/components/Table/Index.js
@@ -1,22 +1,10 @@
 import "./Index.css";
 
-const Table = ({ children, ...props }) => {
-  const productsList = [];
-  async function getProducts() {
-    try {
-      const productsResponse = await fetch(
-        `https://665f28621e9017dc16f31a57.mockapi.io/api/products`
-      );
-      const products = await productsResponse.json();
-      console.log(products);
-    } catch (error) {
-      console.log();
-    }
-  }
-
-  getProducts();
-
-  return <table>{children}</table>;
+const Table = (props) => {
+  return (
+    <table className={props.classNameCustom}>
+      {props.children}
+    </table>
+  );
 };
-
 export default Table;
