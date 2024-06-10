5d24a7bdfb389a5e70a179f95f6b6a710d09689b
Yuriy Baranov <yuri.baranov83@gmail.com>
2024-06-10 17:18:15 +0300

    fix props

--- diploma/src/components/Table/index.js
+++ diploma/src/components/Table/index.js
@@ -1,10 +1,52 @@
 import "./index.css";
+import { PiArrowsDownUpThin } from "react-icons/pi";
+import { BsFillPencilFill } from "react-icons/bs";
+import { RiDeleteBin4Fill } from "react-icons/ri";
 
-const Table = (props) => {
+const Table = ({ classNameCustom, productsList }) => {
   return (
-    <table className={props.classNameCustom}>
-      {props.children}
+    <table className={classNameCustom}>
+      <thead>
+        <tr>
+          <th>
+            ID <PiArrowsDownUpThin />
+          </th>
+          <th>
+            Category
+            <PiArrowsDownUpThin />
+          </th>
+          <th>
+            Name
+            <PiArrowsDownUpThin />
+          </th>
+          <th>
+            Quantity
+            <PiArrowsDownUpThin />
+          </th>
+          <th>
+            Price (₴)
+            <PiArrowsDownUpThin />
+          </th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {productsList.map((element) => (
+          <tr key={element.id}>
+            <td>{element.id}</td>
+            <td>{element.category}</td>
+            <td>{element.name}</td>
+            <td>{element.quantity}</td>
+            <td>{element.price}</td>
+            <td className="edit">
+              <BsFillPencilFill />
+              <RiDeleteBin4Fill />
+            </td>
+          </tr>
+        ))}
+      </tbody>
     </table>
   );
 };
+
 export default Table;
--- diploma/src/pages/Products/Index.js
+++ diploma/src/pages/Products/Index.js
@@ -2,9 +2,6 @@ import "./index.css";
 import { ReactComponent as LogoOther } from "../../assets/logo.svg";
 import { GrUserManager } from "react-icons/gr";
 import { GoPlus } from "react-icons/go";
-import { PiArrowsDownUpThin } from "react-icons/pi";
-import { BsFillPencilFill } from "react-icons/bs";
-import { RiDeleteBin4Fill } from "react-icons/ri";
 import ButtonLogin from "../../components/ButtonLogin";
 import Table from "../../components/Table";
 
@@ -15,6 +12,7 @@ const Products = () => {
     { id: 1, category: "Clothes", name: "Nike M Nk Df Acd21", quantity: 22, price: 4000.0 },
     { id: 2, category: "Plumbing", name: "CERSANIT MITO 17", quantity: 555, price: 5000.0 },
   ];
+
   return (
     <div className="products">
       <div className="row">
@@ -31,49 +29,7 @@ const Products = () => {
         </ButtonLogin>
       </div>
       <h1 className="title">Products</h1>
-      <Table classNameCustom="products-table">
-        <thead>
-          <tr>
-            <th>
-              ID <PiArrowsDownUpThin />
-            </th>
-            <th>
-              Category
-              <PiArrowsDownUpThin />
-            </th>
-            <th>
-              Name
-              <PiArrowsDownUpThin />
-            </th>
-            <th>
-              Quantity
-              <PiArrowsDownUpThin />
-            </th>
-            <th>
-              Price (₴)
-              <PiArrowsDownUpThin />
-            </th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {productsList.map((element) => {
-            return (
-              <tr key={element.id}>
-                <td>{element.id}</td>
-                <td>{element.category}</td>
-                <td>{element.name}</td>
-                <td>{element.quantity}</td>
-                <td>{element.price}</td>
-                <td className="edit">
-                  <BsFillPencilFill />
-                  <RiDeleteBin4Fill />
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </Table>
+      <Table classNameCustom="products-table" productsList={productsList} />
     </div>
   );
 };
