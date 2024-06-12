7383938e58f22e25f38f86b096c5378739748e2e
Yuriy Baranov <yuri.baranov83@gmail.com>
2024-06-12 22:05:52 +0300

    fix hw26

--- diploma/src/components/ProductCard/index.js
+++ diploma/src/components/ProductCard/index.js
@@ -1,27 +1,24 @@
 import "./index.css";
 import { BsCart4 } from "react-icons/bs";
-const ProductCard = ({ ProductPreviewList, readyTodelivery, quantityProd }) => {
+
+const ProductCard = ({ product, readyTodelivery, quantityProd }) => {
   return (
-    <div className="product-card-wrapper">
-      {ProductPreviewList.map((element) => (
-        <div className="product-card" key={element.id}>
-          <div className="image-prod">
-            <img src={element.image} alt="" />
-          </div>
-          <div className="name-prod">{element.name}</div>
-          <div className="color-prod">{element.color}</div>
-          <div className="price-prod">{element.price}</div>
-          <div className="quantity-prod">
-            {quantityProd}
-            {element.quantity}
-          </div>
-          <div className="delivery-prod">
-            <BsCart4 />
-            <span>{readyTodelivery}</span>
-          </div>
+      <div className="product-card" key={product.id}>
+        <div className="image-prod">
+          <img src={product.image} alt="" />
         </div>
-      ))}
-    </div>
+        <div className="name-prod">{product.name}</div>
+        <div className="color-prod">{product.color}</div>
+        <div className="price-prod">{product.price}</div>
+        <div className="quantity-prod">
+          {quantityProd}
+          {product.quantity}
+        </div>
+        <div className="delivery-prod">
+          <BsCart4 />
+          <span>{readyTodelivery}</span>
+        </div>
+      </div>
   );
 };
 
